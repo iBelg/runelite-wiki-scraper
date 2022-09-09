da70a037937c4fcc1fa9b4fc4a19e7e30e72c957
iBelg <4256757+iBelg@users.noreply.github.com>
2022-09-09 16:14:18 +0200

    Extract more data

--- api.py
+++ api.py
@@ -5,60 +5,61 @@ import urllib.parse
 from typing import *
 
 use_cache: bool = True
-user_agent: Dict[str, str] = {"User-Agent": "Runelite Wiki Scraper/1.0 (+abex@runelite.net)"}
+user_agent: Dict[str, str] = {"User-Agent": "Wiki Scraper (iBelg#6474)"}
 
 
 def get_wiki_api(args: Dict[str, str], continueKey: str) -> Iterator[Any]:
-	args["format"] = "json"
-	while True:
-		url = "https://oldschool.runescape.wiki/api.php?" + urllib.parse.urlencode(args)
-		print("Grabbing " + url)
-		with urllib.request.urlopen(urllib.request.Request(url, headers=user_agent)) as raw:
-			js = json.load(raw)
+    args["format"] = "json"
+    while True:
+        url = "https://oldschool.runescape.wiki/api.php?" + \
+            urllib.parse.urlencode(args)
+        print("Grabbing " + url)
+        with urllib.request.urlopen(urllib.request.Request(url, headers=user_agent)) as raw:
+            js = json.load(raw)
 
-		yield js
-		if "continue" in js:
-			args[continueKey] = js["continue"][continueKey]
-		else:
-			return
+        yield js
+        if "continue" in js:
+            args[continueKey] = js["continue"][continueKey]
+        else:
+            return
 
 
 def query_category(category_name: str) -> Dict[str, str]:
-	"""
-	query_category returns a dict of page title to page wikitext
-	you can then use mwparserfromhell to parse the wikitext into
-	an ast
-	"""
-	cache_file_name = category_name + ".cache.json"
-	if use_cache and os.path.isfile(cache_file_name):
-		with open(cache_file_name, "r") as fi:
-			return json.load(fi)
+    """
+    query_category returns a dict of page title to page wikitext
+    you can then use mwparserfromhell to parse the wikitext into
+    an ast
+    """
+    cache_file_name = category_name + ".cache.json"
+    if use_cache and os.path.isfile(cache_file_name):
+        with open(cache_file_name, "r") as fi:
+            return json.load(fi)
 
-	pageids = []
-	for res in get_wiki_api(
-		{
-		"action": "query",
-		"list": "categorymembers",
-		"cmlimit": "500",
-		"cmtitle": "Category:" + category_name,
-		}, "cmcontinue"):
+    pageids = []
+    for res in get_wiki_api(
+            {
+            "action": "query",
+            "list": "categorymembers",
+            "cmlimit": "500",
+            "cmtitle": "Category:" + category_name,
+            }, "cmcontinue"):
 
-		for page in res["query"]["categorymembers"]:
-			pageids.append(str(page["pageid"]))
+        for page in res["query"]["categorymembers"]:
+            pageids.append(str(page["pageid"]))
 
-	pages = {}
-	for i in range(0, len(pageids), 50):
-		for res in get_wiki_api(
-			{
-			"action": "query",
-			"prop": "revisions",
-			"rvprop": "content",
-			"pageids": "|".join(pageids[i:i + 50]),
-			}, "rvcontinue"):
-			for id, page in res["query"]["pages"].items():
-				pages[page["title"]] = page["revisions"][0]["*"]
+    pages = {}
+    for i in range(0, len(pageids), 50):
+        for res in get_wiki_api(
+                {
+                "action": "query",
+                "prop": "revisions",
+                "rvprop": "content",
+                "pageids": "|".join(pageids[i:i + 50]),
+                }, "rvcontinue"):
+            for id, page in res["query"]["pages"].items():
+                pages[page["title"]] = page["revisions"][0]["*"]
 
-	with open(cache_file_name, "w+") as fi:
-		json.dump(pages, fi)
+    with open(cache_file_name, "w+") as fi:
+        json.dump(pages, fi)
 
-	return pages
+    return pages
